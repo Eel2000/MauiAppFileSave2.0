2199794959ba81004c21e2fd39bc399f9e754c70
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2024-02-16 11:42:53 +0200

    updating the solution externals dep

---
+++ Maui/src/CommunityToolkit.Maui.Core/Essentials/FileSaver/FileSaverImplementation.net.cs
@@ -0,0 +1,25 @@
+namespace CommunityToolkit.Maui.Storage;
+
+/// <inheritdoc />
+public sealed partial class FileSaverImplementation : IFileSaver
+{
+	Task<string> InternalSaveAsync(string initialPath, string fileName, Stream stream, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	Task<string> InternalSaveAsync(string initialPath, string fileName, Stream stream, IProgress<double> progress, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	Task<string> InternalSaveAsync(string fileName, Stream stream, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	Task<string> InternalSaveAsync(string fileName, Stream stream, IProgress<double> progress, CancellationToken cancellationToken)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ Maui/src/CommunityToolkit.Maui.Core/Essentials/FileSaver/FileSaverImplementation.windows.cs
@@ -0,0 +1,91 @@
+using System.Diagnostics;
+using Windows.Storage.Pickers;
+
+namespace CommunityToolkit.Maui.Storage;
+
+/// <inheritdoc />
+public sealed partial class FileSaverImplementation : IFileSaver
+{
+	readonly List<string> allFilesExtension = new() { "." };
+
+	async Task<string> InternalSaveAsync(string initialPath, string fileName, Stream stream, CancellationToken cancellationToken)
+	{
+		var savePicker = new FileSavePicker
+		{
+			SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+		};
+		WinRT.Interop.InitializeWithWindow.Initialize(savePicker, Process.GetCurrentProcess().MainWindowHandle);
+
+		var extension = Path.GetExtension(fileName);
+		if (!string.IsNullOrEmpty(extension))
+		{
+			savePicker.FileTypeChoices.Add(extension, new List<string> { extension });
+		}
+
+		savePicker.FileTypeChoices.Add("All files", allFilesExtension);
+		savePicker.SuggestedFileName = Path.GetFileNameWithoutExtension(fileName);
+
+		var filePickerOperation = savePicker.PickSaveFileAsync();
+
+		await using var _ = cancellationToken.Register(CancelFilePickerOperation);
+		var file = await filePickerOperation;
+		if (string.IsNullOrEmpty(file?.Path))
+		{
+			throw new FileSaveException("Operation cancelled or Path doesn't exist.");
+		}
+
+		await WriteStream(stream, file.Path, cancellationToken).ConfigureAwait(false);
+		return file.Path;
+
+		void CancelFilePickerOperation()
+		{
+			filePickerOperation.Cancel();
+		}
+	}
+
+	async Task<string> InternalSaveAsync(string initialPath, string fileName, Stream stream, IProgress<double> progress, CancellationToken cancellationToken)
+	{
+		var savePicker = new FileSavePicker
+		{
+			SuggestedStartLocation = PickerLocationId.DocumentsLibrary
+		};
+		WinRT.Interop.InitializeWithWindow.Initialize(savePicker, Process.GetCurrentProcess().MainWindowHandle);
+
+		var extension = Path.GetExtension(fileName);
+		if (!string.IsNullOrEmpty(extension))
+		{
+			savePicker.FileTypeChoices.Add(extension, new List<string> { extension });
+		}
+
+		savePicker.FileTypeChoices.Add("All files", allFilesExtension);
+		savePicker.SuggestedFileName = Path.GetFileNameWithoutExtension(fileName);
+
+		var filePickerOperation = savePicker.PickSaveFileAsync();
+
+		await using var _ = cancellationToken.Register(CancelFilePickerOperation);
+		var file = await filePickerOperation;
+		if (string.IsNullOrEmpty(file?.Path))
+		{
+			throw new FileSaveException("Operation cancelled or Path doesn't exist.");
+		}
+
+		await WriteStream(stream, file.Path, progress, cancellationToken).ConfigureAwait(false);
+		return file.Path;
+
+		void CancelFilePickerOperation()
+		{
+			filePickerOperation.Cancel();
+		}
+	}
+
+	Task<string> InternalSaveAsync(string fileName, Stream stream, CancellationToken cancellationToken)
+	{
+		return InternalSaveAsync(string.Empty, fileName, stream, cancellationToken);
+	}
+
+	Task<string> InternalSaveAsync(string fileName, Stream stream, IProgress<double> progress, CancellationToken cancellationToken)
+	{
+		return InternalSaveAsync(string.Empty, fileName, stream, progress, cancellationToken);
+	}
+
+}
